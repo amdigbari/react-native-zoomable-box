e39df90e8b087feec05860dd057a29e813987cbd
amir <amirmahdidigbari@yahoo.com>
2019-10-04 02:23:34 +0330

    bugfix

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-/**
- * @format
- */
+import ZoomableBox from "./src/components/ZoomableBox";
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+export default ZoomableBox;
